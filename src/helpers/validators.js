49a2c7726efe67edfc29ef3f9ded033209f50f77
Lyubov Agulova <lu7623@gmail.com>
2024-07-28 14:14:31 +0300

    refactor: refactor validation functions

--- src/helpers/validators.js
+++ src/helpers/validators.js
@@ -30,102 +30,72 @@ const colorLength = (color) => {
     return R.pipe(colorShape, length)
 }
 
+const allGreenLength = colorLength(COLORS.GREEN);
+const allRedLength = colorLength(COLORS.RED);
+const allBlueLength = colorLength(COLORS.BLUE);
+const allOrangeLength = colorLength(COLORS.ORANGE);
+
+
+const isMoreThan2Green = R.pipe(allGreenLength, R.lte(2));
+const isTwoGreen = R.pipe(allGreenLength, R.equals(2));
+const isOneRed = R.pipe(allRedLength, R.equals(1));
+const isFourOrange = R.pipe(allOrangeLength, R.equals(4));
+const isFourGreen = R.pipe(allGreenLength, R.equals(4));
+const isNotRedStar = R.pipe(isRed(SHAPES.STAR), R.not);
+const isNotWhiteStar = R.pipe(isWhite(SHAPES.STAR), R.not);
+const isTriangleAndSquareSame = R.converge(R.equals, [R.prop(SHAPES.TRIANGLE), R.prop(SHAPES.SQUARE)]);
+const isNotWhiteTriangle = R.pipe(isWhite(SHAPES.TRIANGLE), R.not);
+const isBlueEqualsRed = R.converge(R.equals, [allBlueLength, allRedLength]);
+
+const colorsExceptWhite =R.filter(R.pipe( R.equals(COLORS.WHITE), R.not), R.values(COLORS))
+const moreThan3LengthCheckArray = R.map(x => R.pipe(colorLength(x), R.lte(3)), colorsExceptWhite);
+
 
 // 1. Красная звезда, зеленый квадрат, все остальные белые.
 export const validateFieldN1 = (shapes) => {
-  
-    const allPass = R.allPass([isRed(SHAPES.STAR), isGreen(SHAPES.SQUARE), isWhite(SHAPES.TRIANGLE), isWhite(SHAPES.CIRCLE)]);
-
-    return allPass(shapes)
+    return R.allPass([isRed(SHAPES.STAR), isGreen(SHAPES.SQUARE), isWhite(SHAPES.TRIANGLE), isWhite(SHAPES.CIRCLE)])(shapes)
 };
 
 // 2. Как минимум две фигуры зеленые.
 export const validateFieldN2 = (shapes) => {
-  
-    const allGreenLength = colorLength(COLORS.GREEN);
-    const isMoreThan2Green = R.pipe(allGreenLength, R.lte(2));
-    const allPass = R.allPass([isMoreThan2Green]);
-
-    return allPass(shapes)
+    return isMoreThan2Green(shapes)
 };
 
 // 3. Количество красных фигур равно кол-ву синих.
 export const validateFieldN3 =  (shapes) => {
-  
-    const allRedLength = colorLength(COLORS.RED);
-    const allBlueLength = colorLength(COLORS.BLUE);
-    const isBlueEqualsRed = R.converge(R.equals, [allBlueLength, allRedLength]);
-    const allPass = R.allPass([isBlueEqualsRed]);
-
-    return allPass(shapes)
+    return isBlueEqualsRed(shapes)
 };
 
 // 4. Синий круг, красная звезда, оранжевый квадрат треугольник любого цвета
 export const validateFieldN4 =(shapes) => {
-  
-    const allPass = R.allPass([isBlue(SHAPES.CIRCLE),isRed(SHAPES.STAR), isOrange(SHAPES.SQUARE)]);
-
-    return allPass(shapes)
+    return R.allPass([isBlue(SHAPES.CIRCLE),isRed(SHAPES.STAR), isOrange(SHAPES.SQUARE)])(shapes)
 };
 
 // 5. Три фигуры одного любого цвета кроме белого (четыре фигуры одного цвета – это тоже true).
 export const validateFieldN5 =  (shapes) => {
-  
-    const colorsExceptWhite =R.filter(R.pipe( R.equals(COLORS.WHITE), R.not), R.values(COLORS))
-    const moreThan3LengthCheckArray = R.map(x => R.pipe(colorLength(x), R.lte(3)), colorsExceptWhite);
-    const anyPass = R.anyPass(moreThan3LengthCheckArray);
-
-    return anyPass(shapes)
+    return R.anyPass(moreThan3LengthCheckArray)(shapes)
 };
 
 // 6. Ровно две зеленые фигуры (одна из зелёных – это треугольник), плюс одна красная. Четвёртая оставшаяся любого доступного цвета, но не нарушающая первые два условия
 export const validateFieldN6 = (shapes) => {
-  
-    const allGreenLength = colorLength(COLORS.GREEN);
-    const allRedLength = colorLength(COLORS.RED);
-    const isTwoGreen = R.pipe(allGreenLength, R.equals(2));
-    const isOneRed = R.pipe(allRedLength, R.equals(1));
-    const allPass = R.allPass([isTwoGreen, isOneRed, isGreen(SHAPES.TRIANGLE)]);
-
-    return allPass(shapes)
+     return R.allPass([isTwoGreen, isOneRed, isGreen(SHAPES.TRIANGLE)])(shapes)
 };
 // 7. Все фигуры оранжевые.
 export const validateFieldN7 =  (shapes) => {
-  
-    const allOrangeLength = colorLength(COLORS.ORANGE);
-    const isFourOrange = R.pipe(allOrangeLength, R.equals(4));
-    const allPass = R.allPass([isFourOrange]);
-
-    return allPass(shapes)
+    return isFourOrange(shapes)
 };
 
 // 8. Не красная и не белая звезда, остальные – любого цвета.
 export const validateFieldN8 = (shapes) => {
-  
-    const isNotRedStar = R.pipe(isRed(SHAPES.STAR), R.not);
-    const isNotWhiteStar = R.pipe(isWhite(SHAPES.STAR), R.not);
-    const allPass = R.allPass([isNotRedStar, isNotWhiteStar]);
-
-    return allPass(shapes)
+    return R.allPass([isNotRedStar, isNotWhiteStar])(shapes)
 };
 
 // 9. Все фигуры зеленые.
 export const validateFieldN9 = (shapes) => {
-  
-    const allGreenLength = colorLength(COLORS.GREEN);
-    const isFourGreen = R.pipe(allGreenLength, R.equals(4));
-    const allPass = R.allPass([isFourGreen])
-
-    return allPass(shapes)
+  return isFourGreen(shapes)
 };
 
 // 10. Треугольник и квадрат одного цвета (не белого), остальные – любого цвета
-export const validateFieldN10 =  (shapes) => {
-  
-
-    const isSameColor = R.converge(R.equals, [R.prop(SHAPES.TRIANGLE), R.prop(SHAPES.SQUARE)]);
-    const isNotWhite = R.pipe(isWhite(SHAPES.TRIANGLE), R.not)
-    const allPass = R.allPass([isSameColor, isNotWhite]);
-
-    return allPass(shapes)
+export const validateFieldN10 =  (shapes) => {  
+   return R.allPass([isTriangleAndSquareSame, isNotWhiteTriangle])(shapes);
 };
